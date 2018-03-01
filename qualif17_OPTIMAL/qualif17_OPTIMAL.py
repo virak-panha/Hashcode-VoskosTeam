baca9f140645829185d03b66e90a938e2c260d40
gstamatakis <giorgoshstam@gmail.com>
2018-03-01 14:53:49 +0200

    Final update

--- qualif17_OPTIMAL/qualif17_OPTIMAL.py
+++ qualif17_OPTIMAL/qualif17_OPTIMAL.py
@@ -17,43 +17,43 @@ def video_endpoint_cost(videoID, endpoint):
 
 
 def request_cost(req):  # tuples: (video, endpoint, amount of requests)
-    return video_endpoint_cost(req[0], ENDPOINTS[req[1]]) * req[2]
+    return video_endpoint_cost(req[0], ENDPOINTS[req[1]]) * req[2]  # Cost is mul by the amount of requests
 
 
 def how_much_can_we_improve(video):
     _video_index = video[0]  # Get info for the selected video
     _video_size = VIDEOS[_video_index]
 
+    # If there is a best cache for this video
     if BEST_CACHE_CACHE[_video_index] is not None:
         best_cache = BEST_CACHE_CACHE[_video_index]
+        # If the video still 'fits' in the cache then return.
         if _video_size <= CACHE_CAPACITY[best_cache[0]]:
             return best_cache[1], best_cache[0], video
 
     requests = video[1][1]
-
     saved_time = defaultdict(lambda: 0)
+
     for r in requests:  # REQUESTS: (video,endpoint,requests) , ENDPOINTS: datacenterLatency,{cache:cache_latency}
         caches = ENDPOINTS[r[1]][1]  # Available caches for this request
         latency = ENDPOINTS[r[1]][0]  # Datacenter latency
-        # The current latency of this video
+        # The min latency of all the cache items with this video.Also includes datacenter latency just in case.
         current_cost = min([c[1] for c in caches.items() if video[0] in CACHES[c[0]]] + [latency])
 
         # For all the available caches in this endpoint for this video
         for _cache in caches.items():  # caches.items(): (id,latency)
-            space_left = CACHE_CAPACITY[_cache[0]]  # Remaining MBs in cache
-            if _video_size < space_left:  # If this video can fit into this cache
+            if _video_size < CACHE_CAPACITY[_cache[0]]:  # If this video can fit into this cache
                 if current_cost > _cache[1]:  # If we can get lower latency
                     # Add to saved time the latency difference multiplied by the amount of requests
                     saved_time[_cache[0]] += (current_cost - _cache[1]) * r[2]  # r[2] : Number of requests for this vid
 
-    if not saved_time:  # When we can`t reduce latency anymore, optimal solution
+    if not saved_time:  # Latency is optimal, cant get better results
         del vids[video[0]]  # Delete video
-        pbar.update(1)
+        pbar.update(1)  # Progress loading bar
         return 0, None, video
 
     # Pick the cache with the most time saved.
     best_cache = sorted(saved_time.items(), key=lambda x: x[1], reverse=True)[0]
-
     # Save the best cache for this video
     BEST_CACHE_CACHE[_video_index] = best_cache
     # (time saved, cache, video)
@@ -85,7 +85,7 @@ if __name__ == '__main__':
             REQUESTS = []
             CACHES = defaultdict(lambda: set())
             CACHE_CAPACITY = defaultdict(lambda: CacheSize)
-            BEST_CACHE_CACHE = defaultdict(lambda: None)
+            BEST_CACHE_CACHE = defaultdict(lambda: None)  # Best cache for a given video
 
             for _ in range(Endpoints):  # Read info for each Endpoint
                 [datacenterLatency, numOfConCaches] = [int(n) for n in file.readline().split()]
@@ -102,15 +102,17 @@ if __name__ == '__main__':
                 REQUESTS.append(tuple([int(n) for n in file.readline().split()]))
 
             # lambda sets the default value for every new item inserted in the dict. Avoids for-loop for init.
+            # vids contains the cost of each video and its requests. (cost,{requests})
             vids = defaultdict(lambda: [0, []])
-            for req in tqdm(REQUESTS, desc='Requests'):  # tuples: (video endpoint requests)
-                vids[req[0]][0] += request_cost(req)  # vids[video][0] += cost
-                vids[req[0]][1].append(req)  # vids[video][1].append(req)
+            for req in tqdm(REQUESTS, desc='Requests'):  # tuples: (videoID, endpoint, amount of requests)
+                req_video_id = req[0]  # Video ID of this request
+                vids[req_video_id][0] += request_cost(req)  # vids[videoID][0] += cost
+                vids[req_video_id][1].append(req)  # vids[videoID][1].append(req)
 
             pbar = tqdm(total=len(vids))
             pbar.set_description('Videos')
             while vids:
-                # A list of the results of applying the function to the items of the argument sequence
+                # The results of applying the function to the items of the argument sequence
                 vid_list = map(how_much_can_we_improve, list(vids.items()))  # (time saved, cache, video)
                 # Max finds
                 time_saved, cache, video = max(vid_list, key=lambda x: VIDEOS[x[2][0]])
